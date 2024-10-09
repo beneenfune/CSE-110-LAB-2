41980ac291c66432c95e299cf178277469b5dd84
beneenfune <103278030+beneenfune@users.noreply.github.com>
2024-10-09 13:14:44 -0700

    Exercise 4.3.1 CreateDone

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,25 +3,69 @@ import logo from './logo.svg';
 import { Label, Note } from "./types"
 import { dummyNotesList } from './constants';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [notes, setNotes] = useState(dummyNotesList);
+  const initialNote = {
+    id: -1,
+    title: "",
+    content:"",
+    label: Label.other,
+  }
+  const [createNote, setCreateNote] = useState(initialNote);
+
+  function createNoteHandler(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const newNote = {
+      id: notes.length + 1,
+      title: createNote.title,
+      content: createNote.content,
+      label: createNote.label,
+    }
+    setNotes([...notes, newNote]);
+    setCreateNote(initialNote);
+  }
+
   return (
     <div className="app-container">
-      <form className="note-form">
+      <form className="note-form" onSubmit={createNoteHandler}>
         <div>
-          <input placeholder="Note Title"></input>
+          <input 
+            placeholder="Note Title"
+            onChange={(event) =>
+              setCreateNote({...createNote, title: event.target.value})}
+              required>
+          </input>
         </div>
 
         <div>
-          <textarea></textarea>
+          <textarea
+            onChange={(event)=>
+              setCreateNote({...createNote, content: event.target.value})}
+            required>
+          </textarea>
         </div>
 
         <div>
-          <button type="submit">Create Note</button>
+          <select onChange={(event) =>
+            setCreateNote({...createNote, label: event.target.value as Label })} 
+            required>
+              <option value={Label.personal}>Personal</option>
+              <option value={Label.study}>Study</option>
+              <option value={Label.work}>Work</option>
+              <option value={Label.other}>Other</option>
+          </select>
+        </div>
+
+        <div>
+          <button
+           type="submit">Create Note</button>
         </div>
       </form>
+
       <div className="note-grid">
-        {dummyNotesList.map((note) => (
+        {notes.map((note) => (
           <div
             key={note.id}
             className='note-item'>
