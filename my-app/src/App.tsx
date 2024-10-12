538cfc8a6b90081b047916d2b6c7075b22e5afc2
beneenfune <bfune@ucsd.edu>
2024-10-11 23:16:14 -0700

    Exercise toggle theme done

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,12 +5,11 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import { IoHeart } from "react-icons/io5";
 import { IoMdHeartEmpty } from "react-icons/io";
-import { ClickCounter } from "./starterFiles/hooksExercise";
-import ToggleTheme from "./starterFiles/hooksExercise";
+import { ThemeContext, themes } from "./context/ThemeContext";
 
 
 function App() {
-
+  const [currentTheme, setCurrentTheme] = useState(themes.light);
   const [notes, setNotes] = useState(dummyNotesList);
   const initialNote = {
     id: -1,
@@ -21,6 +20,10 @@ function App() {
   const [createNote, setCreateNote] = useState(initialNote);
   const [favorites, setFavorites] = useState<number[]>([]);
   const [selectedNote, setSelectedNote] = useState<Note>(initialNote);
+
+  const toggleTheme = () => {
+    setCurrentTheme(currentTheme === themes.light ? themes.dark : themes.light);
+  };
 
   function createNoteHandler(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -48,7 +51,8 @@ function App() {
 
   useEffect(() => {
     console.log("The favorites list is:", favorites);
-  }, [favorites]);
+      document.body.style.backgroundColor = currentTheme.mainbackgroundcolor;
+  }, [favorites, currentTheme]);
 
   const noteEditor = (field: keyof Note, value: string) => {
     if (selectedNote) {
@@ -64,98 +68,153 @@ function App() {
   };
 
   return (
-    <div className="app-container">
-      <div className="sidebar">
-        <form className="note-form" onSubmit={createNoteHandler}>
-          <div>
-            <input
-              placeholder="Note Title"
-              onChange={(event) =>
-                setCreateNote({ ...createNote, title: event.target.value })
-              }
-              required
-            ></input>
-          </div>
+    <ThemeContext.Provider value={currentTheme}>
+      <div
+        className="app-container"
+        style={{
+          background: currentTheme.mainbackgroundcolor,
+          color: currentTheme.mainfontcolor,
+        }}
+      >
+        <div className="sidebar">
+          <form className="note-form" onSubmit={createNoteHandler}>
+            <div>
+              <input
+                placeholder="Note Title"
+                onChange={(event) =>
+                  setCreateNote({ ...createNote, title: event.target.value })
+                }
+                style={{
+                  color: currentTheme.mainfontcolor,
+                  backgroundColor: currentTheme.inputbackgroundcolor,
+                }}
+                required
+              ></input>
+            </div>
 
-          <div>
-            <textarea
-              placeholder="Note Content"
-              onChange={(event) =>
-                setCreateNote({ ...createNote, content: event.target.value })
-              }
-              required
-            ></textarea>
-          </div>
+            <div>
+              <textarea
+                placeholder="Note Content"
+                onChange={(event) =>
+                  setCreateNote({ ...createNote, content: event.target.value })
+                }
+                style={{
+                  color: currentTheme.mainfontcolor,
+                  backgroundColor: currentTheme.inputbackgroundcolor,
+                }}
+                required
+              ></textarea>
+            </div>
 
-          <div>
-            <select
-              onChange={(event) =>
-                setCreateNote({
-                  ...createNote,
-                  label: event.target.value as Label,
-                })
-              }
-              required
+            <div>
+              <select
+                onChange={(event) =>
+                  setCreateNote({
+                    ...createNote,
+                    label: event.target.value as Label,
+                  })
+                }
+                style={{
+                  color: currentTheme.mainfontcolor,
+                  backgroundColor: currentTheme.inputbackgroundcolor,
+                }}
+                required
+              >
+                <option value={Label.personal}>Personal</option>
+                <option value={Label.study}>Study</option>
+                <option value={Label.work}>Work</option>
+                <option value={Label.other}>Other</option>
+              </select>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                style={{
+                  color: currentTheme.mainfontcolor,
+                }}
+              >
+                Create Note
+              </button>
+            </div>
+          </form>
+
+          <button
+            onClick={toggleTheme}
+            style={{
+              backgroundColor: currentTheme.background,
+              color: currentTheme.mainfontcolor,
+            }}
+          >
+            Toggle Theme
+          </button>
+
+          <div className="favorites">
+            <h2>List of Favorites:</h2>
+            <ul>
+              {favorites.map((favId) => {
+                const favoriteNote = notes.find((note) => note.id === favId);
+                return <li key={favId}>{favoriteNote?.title}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+
+        <div className="note-grid">
+          {notes.map((note) => (
+            <div
+              key={note.id}
+              className="note-item"
+              style={{
+                color: currentTheme.inputfontcolor,
+                backgroundColor: currentTheme.inputbackgroundcolor,
+              }}
             >
-              <option value={Label.personal}>Personal</option>
-              <option value={Label.study}>Study</option>
-              <option value={Label.work}>Work</option>
-              <option value={Label.other}>Other</option>
-            </select>
-          </div>
-
-          <div>
-            <button type="submit">Create Note</button>
-          </div>
-        </form>
-
-        <div className="favorites">
-          <h2>List of Favorites:</h2>
-          <ul>
-            {favorites.map((favId) => {
-              const favoriteNote = notes.find((note) => note.id === favId);
-              return <li key={favId}>{favoriteNote?.title}</li>;
-            })}
-          </ul>
+              <div className="notes-header">
+                <button onClick={() => favoriteClicker(note.id)}>
+                  {favorites.includes(note.id) ? (
+                    <IoHeart style={{ color: "red" }} />
+                  ) : (
+                    <IoMdHeartEmpty />
+                  )}
+                </button>
+                <button onClick={() => deleteClicker(note.id)}>x</button>
+              </div>
+              <h2
+                contentEditable="true"
+                onBlur={(e) =>
+                  noteEditor("title", e.currentTarget.textContent || "")
+                }
+                onClick={() => setSelectedNote(note)}
+                style={{ color: currentTheme.mainfontcolor }}
+              >
+                {note.title}
+              </h2>
+              <p
+                contentEditable="true"
+                onBlur={(e) =>
+                  noteEditor("content", e.currentTarget.textContent || "")
+                }
+                onClick={() => setSelectedNote(note)}
+                style={{ color: currentTheme.mainfontcolor }}
+              >
+                {note.content}
+              </p>
+              <p
+                contentEditable="true"
+                onBlur={(e) =>
+                  noteEditor("label", e.currentTarget.textContent || "")
+                }
+                onClick={() => setSelectedNote(note)}
+                style={{ color: currentTheme.mainfontcolor }}
+              >
+                {note.label}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
-
-      <div className="note-grid">
-        <ToggleTheme/>
-        {notes.map((note) => (
-          <div key={note.id} className="note-item">
-            <div className="notes-header">
-              <button onClick={() => favoriteClicker(note.id)}>
-                {favorites.includes(note.id) ? (
-                  <IoHeart style={{ color: "red" }} />
-                ) : (
-                  <IoMdHeartEmpty />
-                )}
-              </button>
-              <button onClick={ () => deleteClicker(note.id)}>x</button>
-            </div>
-            <h2
-              contentEditable="true"
-              onBlur={(e) => noteEditor('title', e.currentTarget.textContent || '')}
-              onClick={() => setSelectedNote(note)}>
-              {note.title}
-            </h2>
-            <p
-              contentEditable="true"
-              onBlur={(e) => noteEditor('content', e.currentTarget.textContent || '')}
-              onClick={() => setSelectedNote(note)}>
-              {note.content}
-            </p>
-            <p
-              contentEditable="true"
-              onBlur={(e) => noteEditor('label', e.currentTarget.textContent || '')}
-              onClick={() => setSelectedNote(note)}>
-              {note.label}
-            </p>
-          </div>
-        ))}
-      </div>
-    </div>
+    </ThemeContext.Provider>
   );
 }
 
--- my-app/src/context/ThemeContext.ts
+++ my-app/src/context/ThemeContext.ts
@@ -4,10 +4,18 @@ export const themes = {
   light: {
     foreground: "#000000",
     background: "#eeeeee",
+    inputfontcolor: "#333333",
+    inputbackgroundcolor: "#F0F0F0",
+    mainfontcolor: "#1A1A1A",
+    mainbackgroundcolor: "#FFFFFF",
   },
   dark: {
     foreground: "#ffffff",
     background: "#222222",
+    inputfontcolor: "#4A4A4A",
+    inputbackgroundcolor: "#4A4A4A",
+    mainfontcolor: "#E0E0E0",
+    mainbackgroundcolor: "#2C2C2C",
   },
 };
 
